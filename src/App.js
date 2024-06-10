9d80a8738eb32b1d96541ebf3f9390641207d931
Jacob Plumb <jacobmaxplumb@gmail.com>
2024-06-10 19:01:08 -0400

    updated logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
@@ -7,21 +6,37 @@ const animals = ["dog", "cat", "bird", "elephant"];
 const initialState = {
   fullName: '',
   address: '',
-  animals: []
+  animals: ['dog']
 }
 
 function App() {
   const [formValues, setFormValues] = useState(initialState);
+
+  const handleTextChange = (e) => {
+    const {value, id} = e.target;
+    setFormValues({...formValues, [id]: value})
+  }
+
+  const handleCheckboxChange = (e) => {
+    const {name, checked} = e.target;
+    if (checked) {
+      const animals = [...formValues.animals, name];
+      setFormValues({...formValues, animals: animals});
+    } else {
+      const animals = formValues.animals.filter(animal => animal !== name);
+      setFormValues({...formValues, animals: animals});
+    }
+  }
   
   return (
     <>
-      <input placeholder="Full Name" />
+      <input onChange={handleTextChange} id='fullName' placeholder="Full Name" value={formValues.fullName} />
       <br />
-      <input placeholder="Address" />
+      <input onChange={handleTextChange} id='address' placeholder="Address" value={formValues.address} />
       <br />
       {animals.map((animal, index) => (
         <div key={index}>
-          <input type="checkbox" /> {animal}
+          <input name={animal} onChange={handleCheckboxChange} type="checkbox" checked={formValues.animals.includes(animal)} /> {animal}
         </div>
       ))}
       <br />
