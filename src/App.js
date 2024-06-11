3d35c46da9cea32f195d738cc3b3f29fb57ee5ef
Jacob Plumb <jacobmaxplumb@gmail.com>
2024-06-11 18:28:59 -0400

    added schema

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
 import { useState } from "react";
+import * as yup from 'yup';
 
 const animals = ["dog", "cat", "bird", "elephant"];
 
 const initialState = {
   fullName: '',
   address: '',
-  animals: ['dog']
+  animals: []
 }
+
+const schema = yup.object().shape({
+  fullName: yup.string().required('full name is required').min(3, 'full name must be at least 3 characters'),
+  address: yup.string().required('address is required').min(3, 'address must be at least 3 characters'),
+  animals: yup.array()
+  .of(yup.string().oneOf(["dog", "cat", "bird", "elephant"], 'Animal must be one of the following: dog, cat, bird, elephant'))
+  .required('Animals array is required')
+  .min(1, 'At least one animal is required'),
+})
 
 function App() {
   const [formValues, setFormValues] = useState(initialState);
